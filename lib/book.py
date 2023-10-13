065e636a70ce02c9b1619b687ef26139af204cd1
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-10-13 13:52:05 +0300

    Putting It All Together: Object-Oriented Programming Lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None  # Initialize _page_count to None initially
+        self.current_page = 0
+
+        # Set the page_count using the setter method
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        self.current_page += 1
+        print("Flipping the page...wow, you read fast!")
+
+
+
+
+
+
+
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        self._size = value
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+
+
+
+
